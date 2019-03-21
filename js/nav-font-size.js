8cb2e424b09b086a9cd06bc2e62b71c01db51a39
Narayan <nprasath1122591@gmail.com>
2019-03-21 13:52:10 -0700

    daf

--- js/nav-font-size.js
+++ js/nav-font-size.js
@@ -6,6 +6,8 @@ $(function() {
 
       var mass = Math.max(2, 5-0.01*$(this).scrollTop()) + 'vw';
 
-    $('#expandable').css({'font-size': mass, 'line-height': mass});
+    $('#expandable').css({'font-size': mass, 'line-height': mass, 'padding-top': mass, 'font-weight': mass});
+
+
   });
 });
